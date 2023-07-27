0e74565aff80bc5c6ee59028b0b8ffcdb383038d
Alsinapanda <imrankatuba2017@gmail.com>
2023-07-27 23:25:18 +0300

    main

--- main.h
+++ main.h
@@ -14,66 +14,57 @@
 #define F_HASH 8
 #define F_SPACE 16
 
-/* Sizes */
+/* Size */
 #define S_LONG 2
 #define S_SHORT 1
 
-/**
+/*
  * struct fmt - Struct op
- *
- * @fmt: format
- * @fn: function
- * Done by Imran & Fatima
+ * @fmt: format.
+ * @fn: function associated.
  */
 struct fmt
 {
 char fmt;
 int (*fn)(va_list, char[], int, int, int, int);
-};
+}
 
 
 /*
  * typedef struct fmt fmt_t - Struct op
  *
- * @fmt: The format
+ * @fmt: The format.
  * @fm_t: The function
  */
-
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/** Functions **/
-/* Funtions to print characters and strings */
-int print_chars(va_list types, char buffer[], int flags, int width, int precision, int size);
+/* Function */
 
-int print_strings(va_list types, char buffer[], int flags, int width, int precision, int size);
-
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
-
 int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
-
 int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
-
 int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
-
 int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
-
 int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non-printable characters */
+/* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-/* Function to print memory addresses */
+/* Funcion to print memory address */
 int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-/* Functions to handle any other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
@@ -85,16 +76,15 @@ int print_reverse(va_list types, char buffer[], int flags, int width, int precis
 /*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
 
-/* width handlers */
-int handle_write_chars(char c, char buffer[], int flags, int width, int precision, int size);
-
-int write_numbers(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
-
+/* width handler */
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
 int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+int width, int flags, char padd, char extra_c, int padd_start);
 
-int write_pointers(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
-
-int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_unsgnd(int is_negative, int ind, char buffer[],
+int flags, int width, int precision, int size);
 
 /* Utils */
 int is_printable(char);
